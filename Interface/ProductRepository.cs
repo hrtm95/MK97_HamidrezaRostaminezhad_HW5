29d1c47aac8dfd8e698940ae1a12f62e5b616a6b
Hamid reza <h.rostaminezhad@gmail.com>
2023-06-05 00:24:50 +0330

    add CheckProductName in product repository (Q7) mix (Q6)

---
+++ Interface/ProductRepository.cs
@@ -0,0 +1,34 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    public class ProductRepository : IProductRepository
+    {
+        public string AddProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetProductById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetProductList()
+        {
+            throw new NotImplementedException();
+        }
+        private bool CheckProductName(string ProductName)
+        {
+            if (Regex.IsMatch(ProductName, @"^[A-Z]+([a-z]{3})+.+_+([\d]{3})$"))
+                return true;
+            return false;
+        }
+    }
+}
