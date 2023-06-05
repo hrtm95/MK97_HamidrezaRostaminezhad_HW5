ed95987df94f4d7d08847ef874d26385efd9b75e
Hamid reza <h.rostaminezhad@gmail.com>
2023-06-05 17:38:12 +0330

    add path file

---
+++ Database/Paths.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database
+{
+    public static class Paths
+    {
+        public static string product = @"..\..\..\..\Database\Product.Json";
+        public static string stock = @"..\..\..\..\Database\Stock.Json";
+
+    }
+}
