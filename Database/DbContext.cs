036e17af2960b941634ce961d192014861da5c7a
Hamid reza <h.rostaminezhad@gmail.com>
2023-06-05 09:07:39 +0330

    add DbContext.cs for read files

---
+++ Database/DbContext.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database
+{
+    public static class DbContext<T> where T : class
+    {
+        public static bool WriteJson(List<T> list, string path)
+        {
+            try
+            {
+                var Text = JsonConvert.SerializeObject(list);
+                File.WriteAllText(path, Text);
+                return true;
+            }
+            catch
+            {
+                //for any exaption we can throwexeption?? whats beter?
+                return false;
+            }
+        }
+        public static List<T>? ReadJson(string path)
+        {
+            try
+            {
+                string text = File.ReadAllText(path);
+                var list = JsonConvert.DeserializeObject<List<T>>(text);
+                return list;
+            }
+            catch
+            {
+                //for any exaption we can throw exeption?? whats beter?
+                return null;
+            }
+        }
+
+    }
+}
--- MK97_HamidrezaRostaminezhad_HW5/Program.cs
+++ MK97_HamidrezaRostaminezhad_HW5/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Database;
+using Domain;
+
 Console.WriteLine("Hello, World!");
+
+
+//string path = @"..\..\..\..\Database\Product.Json";
+
+//List<Product> products = new List<Product>()
+//{
+
+//    new Product(){ProductId = 1 , Barcode = "barcode1", ProductName = "name1"},
+//    new Product(){ProductId = 2 , Barcode = "barcode2", ProductName = "name2"},
+//    new Product(){ProductId = 3 , Barcode = "barcode3", ProductName = "name3"},
+//    new Product(){ProductId = 4 , Barcode = "barcode4", ProductName = "name4"}
+//};
+//DbContext<Product>.WriteJson(products, path);
+//List<Product>? products =  DbContext<Product>.ReadJson(path);
+//foreach(Product product in products)
+//{
+//    Console.WriteLine(product.ProductName);
+//}
