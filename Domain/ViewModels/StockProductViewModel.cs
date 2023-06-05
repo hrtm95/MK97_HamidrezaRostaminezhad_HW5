d2a43a52cd2175abc01782188ae747f6c49d6872
Hamid reza <h.rostaminezhad@gmail.com>
2023-06-05 11:27:44 +0330

    add ViewModel

---
+++ Domain/ViewModels/StockProductViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.ViewModels
+{
+    public class StockProductViewModel
+    {
+        //stock
+        public int StockId { get; set; }
+        public string StockName { get; set; }
+        public string ProductId { get; set; }
+        public int ProductQuantity { get; set; }
+        public double ProductPrice { get; set; }
+        //product
+        public string ProductName { get; set; }
+        public string Barcode { get; set; }
+    }
+}
--- Interface/IStockRepository.cs
+++ Interface/IStockRepository.cs
@@ -1,7 +1,12 @@
-﻿namespace Interface
+﻿using Domain;
+using Domain.ViewModels;
+
+namespace Interface
 {
     public interface IStockRepository
     {
-
+        string SaleProduct(int productId, int cnt);
+        string BuyProduct(Stock productInStock);
+        List<StockProductViewModel> GetSalesProductList();
     }
 }
